bd26afe27867704e3efb10d3cd6647570f67ac3e
kaXurega <edamame.dev@gmail.com>
2023-02-07 01:40:41 +0900

    Add site-wide library
    config.ts is a collection of text and paths used throughout the site.
    The purpose of this library is to reduce the number of files that need to be changed, so that when changes are made to the site, changes to config.ts will result in changes to how the site is displayed.

---
+++ src/components/config.ts
@@ -0,0 +1,56 @@
+export const config = {
+  about: {
+    main: 'About me',
+    description:
+      'I am currently 24 years old. During the day I work as an employee, but at night I am involved in service creation activities in my field of interest.',
+    text: 'By focusing my wisdom on the inefficiencies that still exist in our society, I work to create efficient services/products. I am currently working on food related issues around me.',
+  },
+  works: {
+    main: 'Works',
+    first: {
+      title: 'project-1',
+      description: 'sample',
+    },
+  },
+  news: {
+    main: 'Latest News',
+  },
+  contact: {
+    main: 'Contact',
+    twitter: {
+      href: 'https://twitter.com/hid3xxx',
+    },
+    github: {
+      href: 'https://github.com/hid3xxx',
+    },
+    discord: {
+      href: '',
+    },
+  },
+  footer: {
+    style: 'mx-2 text-base text-txtColor-gray no-underline hover:text-txtColor-light dark:hover:text-txtColor-dark',
+    main: {
+      name: 'Hid3xxx',
+      href: '/',
+    },
+    about: {
+      name: 'about',
+      href: '/#about',
+    },
+    works: {
+      name: 'works',
+      href: '/#works',
+    },
+    news: {
+      name: 'news',
+      href: '/news',
+    },
+    contact: {
+      name: 'contact',
+      href: '/#contact',
+    },
+  },
+  error: {
+    main: 'Not found',
+  },
+};
