07136a1091d27879db036391f1269f330359ad4b
Hid3xxx <kaxurega@gmail.com>
2023-02-12 18:46:40 +0900

    Improve Age Notation #8 (#9)
    Age is now calculated and automatically changed when a birthday is reached.
    
    Co-authored-by: hid3xxx

--- src/components/config.ts
+++ src/components/config.ts
@@ -1,8 +1,22 @@
+const getAge = () => {
+  const birthday = {
+    year: 1998,
+    month: 3,
+    date: 24,
+  };
+  const currentDate = new Date();
+  const thisYearBirthday = new Date(currentDate.getFullYear(), birthday.month - 1, birthday.date);
+  let age = currentDate.getFullYear() - birthday.year;
+  if (currentDate < thisYearBirthday) {
+    age--;
+  }
+  return age;
+};
+
 export const config = {
   about: {
     main: 'About me',
-    description:
-      'I am currently 24 years old. During the day I work as an employee, but at night I am involved in service creation activities in my field of interest.',
+    description: `I am currently ${getAge()} years old. During the day I work as an employee, but at night I am involved in service creation activities in my field of interest.`,
     text: 'By focusing my wisdom on the inefficiencies that still exist in our society, I work to create efficient services/products. I am currently working on food related issues around me.',
     image: 'https://res.cloudinary.com/dp6bqw1vo/image/upload/v1675691185/portfolio/image_u88j1n.png',
   },
@@ -10,7 +24,8 @@ export const config = {
     main: 'Works',
     first: {
       title: 'nut(tentative)',
-      description: 'The "nut" is a project for the development of optimized snacks with a focus on human appetite and health.',
+      description:
+        'The "nut" is a project for the development of optimized snacks with a focus on human appetite and health.',
       image: 'https://res.cloudinary.com/dp6bqw1vo/image/upload/v1675881696/portfolio/works-first.jpg',
     },
   },
@@ -33,7 +48,7 @@ export const config = {
     name: 'Hid3xxx',
     href: '/',
   },
-  links : [
+  links: [
     { name: 'about', href: '/#about' },
     { name: 'works', href: '/#works' },
     { name: 'news', href: '/news' },
