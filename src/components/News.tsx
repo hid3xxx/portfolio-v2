b8286f0fbaabe172dccd3bf31c289e10ef937bbb
Hid3xxx <kaxurega@gmail.com>
2023-02-14 23:45:03 +0900

    Improve: Prettier rules (#11)
    * Update prettier settings #10
    
    * Fix code formatting
    
    The code has been formatted according to the rules set in Prettier.
    
    ---------
    
    Co-authored-by: hid3xxx

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -4,8 +4,11 @@ export const News = (): JSX.Element => {
   return (
     <section id="news" className="flex-grow">
       <h1 className="text-3xl font-bold">{config.news.main}</h1>
-      <div className="md:flex md:flex-col lg:flex lg:w-1/2 lg:space-x-4 py-6">
-        <a className="twitter-timeline" href="https://twitter.com/hid3xxx?ref_src=twsrc%5Etfw">
+      <div className="py-6 md:flex md:flex-col lg:flex lg:w-1/2 lg:space-x-4">
+        <a
+          className="twitter-timeline"
+          href="https://twitter.com/hid3xxx?ref_src=twsrc%5Etfw"
+        >
           Tweets by hid3xxx
         </a>
         <script async src="https://platform.twitter.com/widgets.js" />
